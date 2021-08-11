4213d8a6b27a23effb761a00dfe253b13fa05508
Leandro Meyer <leandro.padrepauloricardo@gmail.com>
2021-08-10 23:21:52 -0300

    second

--- index.js
+++ index.js
@@ -7,20 +7,57 @@ bola.addEventListener('click', function() {
       return response.json();
     })
     .then(function(data) {
-      // Data é a lista de dados
-      // Então, cada item da lista de dados
-      // pode ser uma linha da tabela
-      // laço de repetição
       data.forEach(element => {
-        console.log(element);
+       // console.log(element);
+
+
+        var table = document.createElement('table');
+        table.style = 'width:500px;border:1px solid #CCC;';
+        var tbody = document.createElement('tbody');
+            let tr = document.createElement('tr');
+        
+            // 1
+            let td = document.createElement('td');
+            td.style = 'width:100px;border:1px solid #CCC;';
+            let span = document.createElement('span');
+            span.innerHTML = element.date;
+            td.appendChild(span);
+            tr.appendChild(td);
+        
+            // 2
+            td = document.createElement('td');
+            td.style = 'border:1px solid #CCC;';
+            span = document.createElement('span');
+            span.innerHTML = element.weekday;
+            td.appendChild(span);
+            tr.appendChild(td);
+            tbody.appendChild(tr);
+        
+        table.appendChild(tbody);
+        document.body.appendChild(table);
+
       });
-      // criar um item na minha tabela
-      // alert(data.celebrations[0].title);
     })
+    
+    fetch('http://calapi.inadiutorium.cz/api/v0/en/calendars/general-en/today')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data2) {
+      data2.forEach(element => {
+        console.log(element);
+
+      });
+    })
+    
+
+
     .catch(function(data) {
       console.log('Deu erro')
     });
-})
+    
+
+  })
 
 // array = lista
 
@@ -31,3 +68,18 @@ bola.addEventListener('click', function() {
 
 // http://calapi.inadiutorium.cz/api/v0/en/calendars/general-en/2021/08
 // http://calapi.inadiutorium.cz/api/v0/en/calendars/general-en/today
+
+
+
+      // Data é a lista de dados
+      // Então, cada item da lista de dados
+      // pode ser uma linha da tabela
+      // laço de repetição
+      /*
+      Aqui ele puxa a lista completa
+      data.forEach(element => {
+        console.log(element);
+      });
+      */
+      // criar um item na minha tabela
+      // alert(data.celebrations[0].title);
